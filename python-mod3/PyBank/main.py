a9664c9fb492871f72e718a30ab0be4d9d93c83d
sasidhy1 <sasidhy1@gmail.com>
2018-08-23 12:13:56 -0400

    Store profit/loss changes in list, calculate and output average. Note: first change calculated is erroneous, removed from list before taking sum and length.

--- python-mod3/PyBank/main.py
+++ python-mod3/PyBank/main.py
@@ -3,6 +3,8 @@ import csv
 
 net = 0
 count = 0
+prev = 0
+arr = []
 
 csvpath = os.path.join('budget_data.csv')
 output = os.path.join('output.txt')
@@ -18,7 +20,22 @@ with open(csvpath,newline='') as csvfile:
 		# net amount of profit/losses over entire period
 		net = int(row[1]) + net
 
+		# average profit/losses between months
+		change = int(row[1]) - prev
+		arr.append(change)	# store into array
+		prev = int(row[1])	# update previous value
+
+# print calculated data to terminal
 print("Financial Analysis")
 print("---------------------------------")
+
+# total number of months in dataset
 print(f"months: {count}")
+
+# net amount of profit/losses over entire period
 print(f"total: ${net}")
+
+# average profit/losses between months
+arr.pop(0)	# remove erroneous calculation
+avg = round(sum(arr)/len(arr),2)
+print(f"average: ${avg}")
