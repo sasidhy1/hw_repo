0c62a6ffc637db16a8f5c3fe3c83eb11d7b02618
sasidhy1 <sasidhy1@gmail.com>
2018-08-23 20:19:08 -0400

    Add comments, restructured program and defined function for printing financial analysis.

--- python-mod3/PyBank/main.py
+++ python-mod3/PyBank/main.py
@@ -1,16 +1,35 @@
+# import modules
 import os
 import csv
 
+# initialize variables
 net = 0
 count = 0
+avg = 0
 arr = []
 prev = 0
 biggest = 0
 smallest = 0
 
+# declare paths for I/O data
 csvpath = os.path.join('budget_data.csv')
 output = os.path.join('output','analysis.txt')
 
+def fin_analysis(count,net,arr,datemax,datemin,biggest,smallest):
+	# total number of months, net profit/losses
+	print(f"months: {count}")
+	print(f"total: ${net}")
+
+	# average profit/losses
+	arr.pop(0)							# remove erroneous calculation
+	avg = round(sum(arr)/len(arr),2)	# calculate average
+	print(f"average: ${avg}")
+
+	# greatest in/decrease, date + amount
+	print(f"greatest increase: {datemax} ({biggest})")
+	print(f"greatest decrease: {datemin} ({smallest})")
+
+# perform calculations, store and update min/max vals
 with open(csvpath,newline='') as csvfile:
 	csvreader = csv.reader(csvfile,delimiter=',')
 	header = next(csvreader)
@@ -37,27 +56,12 @@ with open(csvpath,newline='') as csvfile:
 			smallest = change
 			datemin = row[0]
 
-# print calculated data to terminal
+# print financial analysis
 print("Financial Analysis")
 print("---------------------------------")
+fin_analysis(count,net,arr,datemax,datemin,biggest,smallest)
 
-# total number of months in dataset
-print(f"months: {count}")
-
-# net amount of profit/losses over entire period
-print(f"total: ${net}")
-
-# average profit/losses between months
-arr.pop(0)	# remove erroneous calculation
-avg = round(sum(arr)/len(arr),2)
-print(f"average: ${avg}")
-
-# greatest profit increase, date and amount
-print(f"greatest increase: {datemax} ({biggest})")
-
-# greatest loss decrease, date and amount
-print(f"greatest decrease: {datemin} ({smallest})")
-
+# output results to textfile
 with open(output,'w') as text:
 	text.write("Financial Analysis" + "\n")
 	text.write("---------------------------------\n")
