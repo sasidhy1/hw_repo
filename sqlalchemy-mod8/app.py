9a9d74aff0bc051c978691f8baf0676240ea835e
sasidhy1 <sasidhy1@gmail.com>
2018-10-20 03:44:02 -0400

    Add query for tobs page.

--- sqlalchemy-mod8/app.py
+++ sqlalchemy-mod8/app.py
@@ -22,7 +22,7 @@ Measurement = Base.classes.measurement
 Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-session = Session(engine)
+
 
 ###########################
 ##### FLASK API SETUP #####
@@ -53,19 +53,19 @@ def index():
 def prcp():
 	print('Server received request for prcp page...')
 	
-	end_date = session.query(Measurement.date).\
-		order_by(Measurement.date.desc()).first()[0]
+	# end_date = session.query(Measurement.date).\
+	# 	order_by(Measurement.date.desc()).first()[0]
 	
 	# dat = [int(n) for n in end_date[0].split('-')]
 	# y = dt.date(*dat).strftime("%Y")
 	# m = dt.date(*dat).strftime("%m")
 	# d = dt.date(*dat).strftime("%d")
-
+	session = Session(engine)
 	yr_ago = dt.date(2017-1,8,23)
 
 	q = session.query(Measurement.date,Measurement.prcp).\
-    	filter(Measurement.date > yr_ago).\
-    	order_by(Measurement.date).all()
+		filter(Measurement.date > yr_ago).\
+		order_by(Measurement.date).all()
 
 	all_data = []
 	for i in q:
@@ -78,17 +78,35 @@ def prcp():
 @app.route('/api/v1.0/stations')
 def stat():
 	print('Server received request for stat page...')
+	session = Session(engine)
+	q1 = session.query(Station.name).all()
 
-	q = session.query(Station.name).all()
-
-	disp = list(np.ravel(q))
-
+	disp = list(np.ravel(q1))
 	return jsonify(disp)
 
 @app.route('/api/v1.0/tobs')
 def tobs():
 	print('Server received request for tobs page...')
+	session = Session(engine)
+	yr_ago = dt.date(2017-1,8,23)
 
+	sel = [Measurement.station,
+		Measurement.date,
+		Measurement.tobs]
+
+	q2 = session.query(*sel).\
+		filter(Station.station == Measurement.station).\
+		filter(Station.station == 'USC00519281').\
+		filter(Measurement.date > yr_ago).\
+		order_by(Measurement.date).all()
+
+	all_data = []
+	for i in q2:
+		this_dict = {}
+		this_dict[i[1]] = i[2]
+		all_data.append(this_dict)
+
+	return jsonify(all_data)
 
 @app.route('/api/v1.0/<start>')
 def given_start(start):
