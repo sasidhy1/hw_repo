fe0e22b37f1cde889cc4a65bac908959e6b199b7
sasidhy1 <sasidhy1@gmail.com>
2018-10-20 10:54:10 -0400

    Write queries for supplied start and end date variables.

--- sqlalchemy-mod8/app.py
+++ sqlalchemy-mod8/app.py
@@ -6,7 +6,7 @@
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
+from sqlalchemy import create_engine,func
 
 # Specify db location
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
@@ -63,12 +63,12 @@ def prcp():
 	session = Session(engine)
 	yr_ago = dt.date(2017-1,8,23)
 
-	q = session.query(Measurement.date,Measurement.prcp).\
+	query = session.query(Measurement.date,Measurement.prcp).\
 		filter(Measurement.date > yr_ago).\
 		order_by(Measurement.date).all()
 
 	all_data = []
-	for i in q:
+	for i in query:
 		this_dict = {}
 		this_dict[i[0]] = i[1]
 		all_data.append(this_dict)
@@ -79,9 +79,9 @@ def prcp():
 def stat():
 	print('Server received request for stat page...')
 	session = Session(engine)
-	q1 = session.query(Station.name).all()
+	query = session.query(Station.name).all()
 
-	disp = list(np.ravel(q1))
+	disp = list(np.ravel(query))
 	return jsonify(disp)
 
 @app.route('/api/v1.0/tobs')
@@ -94,14 +94,14 @@ def tobs():
 		Measurement.date,
 		Measurement.tobs]
 
-	q2 = session.query(*sel).\
+	query = session.query(*sel).\
 		filter(Station.station == Measurement.station).\
 		filter(Station.station == 'USC00519281').\
 		filter(Measurement.date > yr_ago).\
 		order_by(Measurement.date).all()
 
 	all_data = []
-	for i in q2:
+	for i in query:
 		this_dict = {}
 		this_dict[i[1]] = i[2]
 		all_data.append(this_dict)
@@ -110,12 +110,55 @@ def tobs():
 
 @app.route('/api/v1.0/<start>')
 def given_start(start):
-	print('Server received request for temp range page...')
+	session = Session(engine)
+	print('Server received request for supplied path variable...')
 
+	session = Session(engine)
+
+	sel = [func.min(Measurement.tobs),
+		func.max(Measurement.tobs),
+		func.avg(Measurement.tobs)]
+
+	query = session.query(*sel).\
+		filter(Station.station == Measurement.station).\
+		filter(Measurement.date > start).\
+		filter(Station.station == 'USC00519281').all()
+
+	all_data = []
+	for i in query:
+		this_dict = {}
+		this_dict['min'] = i[0]
+		this_dict['max'] = i[1]
+		this_dict['avg'] = i[2]
+		all_data.append(this_dict)
+
+	return jsonify(all_data)
 
 @app.route('/api/v1.0/<start>/<end>')
 def given_start_and_end(start,end):
-	print('Server received request for temp range page...')
+	print('Server received request for supplied path variables...')
+
+	session = Session(engine)
+
+	sel = [func.min(Measurement.tobs),
+		func.max(Measurement.tobs),
+		func.avg(Measurement.tobs)]
+
+	query = session.query(*sel).\
+		filter(Station.station == Measurement.station).\
+		filter(Measurement.date > start).\
+		filter(Measurement.date < end).\
+		filter(Station.station == 'USC00519281').all()
+
+	all_data = []
+	for i in query:
+		this_dict = {}
+		this_dict['min'] = i[0]
+		this_dict['max'] = i[1]
+		this_dict['avg'] = i[2]
+		all_data.append(this_dict)
+
+	return jsonify(all_data)
 
 if __name__ == '__main__':
 	app.run(debug=True)
