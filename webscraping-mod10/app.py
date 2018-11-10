8be9cf42019208f06d958dd5fc6143e77ef9ca23
sasidhy1 <sasidhy1@gmail.com>
2018-11-10 18:03:19 -0500

    Add print statements for log.

--- webscraping-mod10/app.py
+++ webscraping-mod10/app.py
@@ -11,18 +11,32 @@ mongo = PyMongo(app, uri='mongodb://localhost:27017/mars_app')
 # define static routes
 @app.route('/')
 def index():
-	print('Server received request.')
 
+	# grab existing collection as dict
+	print('Looking for existing collection...',end='')
 	mars_data = mongo.db.collection.find_one()
-	
+	print('done.')
+
+	# render html template, passing dictionary
 	return render_template('index.html',mars_data=mars_data)
 
 @app.route('/scrape')
 def scrape():
-	scraped_data = scrape_mars.scrape()
 
+	# run scrape function, store as dict
+	print('Scraping in progress...',end='')
+	scraped_data = scrape_mars.scrape()
+	print('done.')
+
+	# drop previous scrape
+	print('Dropping old collection...',end='')
 	mongo.db.collection.drop()
+	print('done.')
+
+	# insert current scrape
+	print('Rendering scraped data...',end='')
 	mongo.db.collection.insert_one(scraped_data)
+	print('done.')
 
 	return redirect('/',code=302)
 
