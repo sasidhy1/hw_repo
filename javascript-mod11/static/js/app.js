537785fd364a35cf4b93532012a4b9145e5acc8c
sasidhy1 <sasidhy1@gmail.com>
2018-11-16 01:37:48 -0500

    Add working filters for specified object keys, need to modify for overlapping figures.

--- javascript-mod11/static/js/app.js
+++ javascript-mod11/static/js/app.js
@@ -1,6 +1,8 @@
 // from data.js
 var tableData = data;
 
+var arr = ['#datetime','#city','#state','#country','#shape'];
+
 // select filter button
 var submit = d3.select('#filter-btn');
 
@@ -9,22 +11,29 @@ submit.on('click', function() {
 	// prevent form reset
 	d3.event.preventDefault();
 
-	// select input form
-	var inputElement = d3.select('#datetime');
+	// clear table before query
+	d3.select('.table-data>tr').remove();
 
-	// return entered value
-	var inputValue = inputElement.property('value');
+	arr.forEach(filterType => {
+	
+		// select input form
+		var inputElement = d3.select(filterType);
 
-	// filter data for matching datetime
-	var filteredData = data.filter(entry => entry.datetime == inputValue);
+		// return entered value
+		var inputValue = inputElement.property('value');
 
-	// create new row for each entry
-	filteredData.forEach(entry => {
-		var newRow = d3.select('.table-data').append('tr');
+		// filter data for matching datetime
+		var filteredData = data.filter(entry => entry[filterType.slice(1)] == inputValue);
 
-		// enter table data for each value
-		Object.values(entry).forEach(value => {
-			newRow.append('td').text(value);
+		// create new row for each entry
+		filteredData.forEach(entry => {
+			var newRow = d3.select('.table-data').append('tr');
+
+			// enter table data for each value
+			Object.values(entry).forEach(value => {
+				newRow.append('td').text(value);
+			});
+
 		});
 		
 	});
