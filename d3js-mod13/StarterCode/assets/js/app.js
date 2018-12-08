96f6d381a71452462ced9fc12547ff583e5b3647
sasidhy1 <sasidhy1@gmail.com>
2018-12-08 00:16:41 -0500

    Load csv, print data to console.

--- d3js-mod13/StarterCode/assets/js/app.js
+++ d3js-mod13/StarterCode/assets/js/app.js
@@ -1,1 +1,12 @@
 // @TODO: YOUR CODE HERE!
+
+path = 'assets/data/data.csv';
+
+d3.csv(path, (error, data) => {
+
+	// log error if exists
+	if (error) return console.warn(error);
+	
+	// print data to console
+	console.log(data);
+});
