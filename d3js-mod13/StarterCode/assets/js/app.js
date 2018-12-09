cd292f12b9f21e8419a5ed356758bc85cc2bdd1e
sasidhy1 <sasidhy1@gmail.com>
2018-12-09 02:22:28 -0500

    Add initial markers to scatter plot.

--- d3js-mod13/StarterCode/assets/js/app.js
+++ d3js-mod13/StarterCode/assets/js/app.js
@@ -1,31 +1,3 @@
-var svgWidth = 960;
-var svgHeight = 500;
-
-var margin = {
-  top: 20,
-  right: 40,
-  bottom: 80,
-  left: 100
-};
-
-var width = svgWidth - margin.left - margin.right;
-var height = svgHeight - margin.top - margin.bottom;
-
-// Create an SVG wrapper, append an SVG group that will hold our chart,
-// and shift the latter by left and top margins.
-var svg = d3
-  .select("#scatter")
-  .append("svg")
-  .attr("width", svgWidth)
-  .attr("height", svgHeight);
-
-// Append an SVG group
-var chartGroup = svg.append("g")
-  .attr("transform", `translate(${margin.left}, ${margin.top})`);
-
-var chosenXAxis = "poverty";	// age, income
-var chosenYAxis = "obesity";	// smokes, noHealthInsurance
-
 // function used for updating x-scale var upon click on axis label
 function xScale(data, chosenXAxis) {
 	// create scales
@@ -47,60 +19,86 @@ function yScale(data, chosenYAxis) {
 
 // function used for updating xAxis var upon click on axis label
 function renderAxes(newXScale, xAxis) {
-  var bottomAxis = d3.axisBottom(newXScale);
+	var bottomAxis = d3.axisBottom(newXScale);
 
-  xAxis.transition()
-    .duration(1000)
-    .call(bottomAxis);
+	xAxis.transition()
+		.duration(1000)
+		.call(bottomAxis);
 
-  return xAxis;
+	return xAxis;
 }
 
 // function used for updating circles group with a transition to
 // new circles
 function renderCircles(circlesGroup, newXScale, chosenXaxis) {
 
-  circlesGroup.transition()
-    .duration(1000)
-    .attr("cx", d => newXScale(d[chosenXAxis]));
+	circlesGroup.transition()
+		.duration(1000)
+		.attr("cx", d => newXScale(d[chosenXAxis]));
 
-  return circlesGroup;
+	return circlesGroup;
 }
 
 // function used for updating circles group with new tooltip
 function updateToolTip(chosenXAxis, circlesGroup) {
 
-  if (chosenXAxis === "poverty") {
-    var label = "Poverty %:";
-  } else if (chosenXAxis === "age") {
-    var label = "Median age:";
-  } else {
-    var label = "Household Income:";
-  }
+	if (chosenXAxis === "poverty") {
+		var label = "Poverty %:";
+	} else if (chosenXAxis === "age") {
+		var label = "Median age:";
+	} else {
+		var label = "Household Income:";
+	}
 
-  var toolTip = d3.tip()
-    .attr("class", "tooltip")
-    .offset([80, -60])
-    .html(function(d) {
-      return (`${d.state}<br>${label}: ${d[chosenXAxis]}`);
-    });
+	var toolTip = d3.tip()
+		.attr("class", "tooltip")
+		.offset([80, -60])
+		.html(function(d) {
+			return (`${d.state}<br>${label}: ${d[chosenXAxis]}`);
+		});
 
-  circlesGroup.call(toolTip);
+	circlesGroup.call(toolTip);
 
-  circlesGroup.on("mouseover", function(data) {
-    toolTip.show(data);
-  })
-    // onmouseout event
-    .on("mouseout", function(data, index) {
-      toolTip.hide(data);
-    });
+	circlesGroup.on("mouseover", function(data) {
+		toolTip.show(data);
+	})
+		// onmouseout event
+		.on("mouseout", function(data, index) {
+			toolTip.hide(data);
+		});
 
-  return circlesGroup;
+	return circlesGroup;
 }
 
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+	top: 20,
+	right: 40,
+	bottom: 80,
+	left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart,
+// and shift the latter by left and top margins.
+var svg = d3
+	.select("#scatter")
+	.append("svg")
+	.attr("width", svgWidth)
+	.attr("height", svgHeight);
+
+// Append an SVG group
+var chartGroup = svg.append("g")
+	.attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+var chosenXAxis = "poverty";	// age, income
+var chosenYAxis = "obesity";	// smokes, noHealthInsurance
 
 path = 'assets/data/data.csv';
-
 d3.csv(path, (error, data) => {
 
 	// log error if exists
@@ -142,5 +140,18 @@ d3.csv(path, (error, data) => {
 	var yAxis = chartGroup.append("g")
 		.classed("y-axis", true)
 		.call(leftAxis);
+
+	// append initial circles
+	var circlesGroup = chartGroup.selectAll("circle")
+		.data(data)
+		.enter()
+		.append("circle")
+		.attr("cx", d => xLinearScale(d[chosenXAxis]))
+		.attr("cy", d => yLinearScale(d[chosenYAxis]))
+		.attr("r", 12)
+		.attr("stroke", "whitesmoke")
+		.attr("stroke-width", 1)
+		.attr("fill", "lightblue")
+		.attr("opacity", 0.95)
 
 });
