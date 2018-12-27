764de4fb6221218a4a05283ee97c18ad59690608
sasidhy1 <sasidhy1@gmail.com>
2018-12-27 00:27:16 -0500

    Create circle markers for each feature, use layermaps.

--- geomap-mod14/static/js/logic.js
+++ geomap-mod14/static/js/logic.js
@@ -1,21 +1,62 @@
-// Creating map object
-var map = L.map("map", {
-  center: [40, -100],
-  zoom: 5
-});
-
-// Adding tile layer
-L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 18,
-  id: "mapbox.streets",
-  accessToken: API_KEY
-}).addTo(map);
-
 var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-// Grabbing our GeoJSON data..
 d3.json(link, function(data) {
-  // Creating a GeoJSON layer with the retrieved data
-  L.geoJson(data).addTo(map);
+  createFeatures(data.features);
 });
+
+function createFeatures(earthquakeData) {
+
+  function onEachFeature(feature, layer) {
+    layer.bindPopup("<h3>" + feature.properties.place +
+      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+  }
+
+  var earthquakes = L.geoJSON(earthquakeData, {
+    onEachFeature: onEachFeature,
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, {
+			radius: feature.properties.mag*4,
+			fillColor: "#ff7800",
+			weight: 1,
+			opacity: 1,
+			fillOpacity: 0.8
+        });
+    }
+  });
+
+  createMap(earthquakes);
+}
+
+function createMap(earthquakes) {
+
+  var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    maxZoom: 18,
+    id: "mapbox.streets",
+    accessToken: API_KEY
+  });
+
+  var darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    maxZoom: 18,
+    id: "mapbox.dark",
+    accessToken: API_KEY
+  });
+
+  var baseMaps = {
+    "Street Map": streetmap,
+    "Dark Map": darkmap
+  };
+
+  var overlayMaps = {
+    Earthquakes: earthquakes
+  };
+
+  var map = L.map("map", {
+	center: [40, -100],
+	zoom: 4.1,
+    layers: [streetmap, earthquakes]
+  });
+
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(map);
+}
