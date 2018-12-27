cc3da35260065ddedab10d0aed2fea99dbc74b5f
sasidhy1 <sasidhy1@gmail.com>
2018-12-27 00:33:31 -0500

    Use switch case to color markers by floor value.

--- geomap-mod14/static/js/logic.js
+++ geomap-mod14/static/js/logic.js
@@ -1,3 +1,20 @@
+function magColor(val) {
+  switch (Math.floor(val)) {
+    case 0: color = "#B7F34D";
+      break;
+    case 1: color = "#E1F34D";
+      break;
+    case 2: color = "#F3DB4D";
+      break;
+    case 3: color = "#F3BA4D";
+      break;
+    case 4: color = "#EE834F";
+      break;
+    case 5: color = "#EC4646";
+  }
+  return color;
+}
+
 var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 d3.json(link, function(data) {
@@ -16,8 +33,9 @@ function createFeatures(earthquakeData) {
     pointToLayer: function (feature, latlng) {
         return L.circleMarker(latlng, {
 			radius: feature.properties.mag*4,
-			fillColor: "#ff7800",
-			weight: 1,
+			fillColor: magColor(feature.properties.mag),
+			color: "#000",
+			weight: 0.5,
 			opacity: 1,
 			fillOpacity: 0.8
         });
@@ -41,16 +59,23 @@ function createMap(earthquakes) {
     accessToken: API_KEY
   });
 
+  var satellitemap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    maxZoom: 18,
+    id: "mapbox.satellite",
+    accessToken: API_KEY
+  });
+
   var baseMaps = {
     "Street Map": streetmap,
-    "Dark Map": darkmap
+    "Dark Map": darkmap,
+    "Satellite Map": satellitemap
   };
 
   var overlayMaps = {
     Earthquakes: earthquakes
   };
 
-  var map = L.map("map", {
+  var myMap = L.map("map", {
 	center: [40, -100],
 	zoom: 4.1,
     layers: [streetmap, earthquakes]
@@ -58,5 +83,5 @@ function createMap(earthquakes) {
 
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
-  }).addTo(map);
+  }).addTo(myMap);
 }
